824c21ac0f89f3c5a08ccebf583a44052cdce7ea
rubac <rl.bach@posteo.de>
2023-09-12 16:22:31 -0400

    htgfdsa

--- 1_data_prep.R
+++ 1_data_prep.R
@@ -53,45 +53,67 @@ df_threebox <- df_threebox %>%
   mutate(answerbox = case_when(
     answerbox=="lhappro1_c2" ~ "one",
     answerbox=="lhappro2_c2" ~ "two",
-    answerbox=="lhappro3_c2" ~ "three")) %>% 
+    answerbox=="lhappro3_c2" ~ "three"))
 
 df_threebox_1 <- df_threebox %>% 
-  filter(answerbox=="one")
+  filter(answerbox=="one") %>% 
+  select(lfdn, lhap_ec, answerbox, text, lseh2_11:Iseh2_14) %>% 
+  rename(label_1 = lseh2_11,
+         label_2 = lseh2_12,
+         label_3 = Iseh2_13,
+         label_4 = Iseh2_14)
+df_threebox_2 <- df_threebox %>% 
+  filter(answerbox=="two") %>% 
+  select(lfdn, lhap_ec, answerbox, text, lseh2_21:lseh2_22) %>% 
+  rename(label_1 = lseh2_21,
+         label_2 = lseh2_22) %>% 
+  mutate(label_3 = NA,
+         label_4 = NA)
+df_threebox_3 <- df_threebox %>% 
+  filter(answerbox=="three") %>% 
+  select(lfdn, lhap_ec, answerbox, text, lseh2_31:lseh2_32) %>% 
+  rename(label_1 = lseh2_31,
+         label_2 = lseh2_32) %>% 
+  mutate(label_3 = NA,
+         label_4 = NA)
   
-  
-  
-df_threebox3 <- gather(df_threebox, answerbox, text, lhappro1_c2:lhappro3_c2, factor_key=TRUE)
+df_threebox <- rbind(df_threebox_1,df_threebox_2,df_threebox_3)
+rm(df_threebox_1,df_threebox_2,df_threebox_3)
 
 
-names(df_threebox2)
-table(df_threebox2$answerbox)
+df_fivebox <- df_fivebox %>% 
+  select(lfdn, lhap_ec, lhappro1_c3:lhappro5_c3, lseh3_11:lseh3_52)
 
-df_threebox$label_1 <- haven::as_factor(df_threebox$lseh1_1)
-df_threebox$label_2 <- haven::as_factor(df_threebox$lseh1_2)
-df_threebox$label_3 <- haven::as_factor(df_threebox$lseh1_3)
-df_threebox$label_4 <- haven::as_factor(df_threebox$lseh1_4)
-df_threebox$label_5 <- haven::as_factor(df_threebox$lseh1_5)
-df_threebox$label_6 <- haven::as_factor(df_threebox$lseh1_6)
-df_threebox$label_7 <- haven::as_factor(df_threebox$lseh1_7)
-df_threebox$label_8 <- haven::as_factor(df_threebox$lseh1_8)
-df_threebox$label_9 <- haven::as_factor(df_onebox$lseh1_9)
-names(df_onebox)
+df_fivebox <- gather(df_fivebox, answerbox, text, lhappro1_c3:lhappro5_c3, factor_key=TRUE)
+df_fivebox <- df_fivebox %>% 
+  mutate(answerbox = case_when(
+    answerbox=="lhappro1_c3" ~ "one",
+    answerbox=="lhappro2_c3" ~ "two",
+    answerbox=="lhappro3_c3" ~ "three",
+    answerbox=="lhappro4_c3" ~ "four",
+    answerbox=="lhappro5_c3" ~ "five"))
 
-lseh2_11"                   "lseh2_12"                  
- [57] "Iseh2_13"                   "Iseh2_14"                   "lseh2_21"                   "lseh2_22"                  
- [61] "lseh2_31"                   "lseh2_32"
+df_threebox_1 <- df_threebox %>% 
+  filter(answerbox=="one") %>% 
+  select(lfdn, lhap_ec, answerbox, text, lseh2_11:Iseh2_14) %>% 
+  rename(label_1 = lseh2_11,
+         label_2 = lseh2_12,
+         label_3 = Iseh2_13,
+         label_4 = Iseh2_14)
+df_threebox_2 <- df_threebox %>% 
+  filter(answerbox=="two") %>% 
+  select(lfdn, lhap_ec, answerbox, text, lseh2_21:lseh2_22) %>% 
+  rename(label_1 = lseh2_21,
+         label_2 = lseh2_22) %>% 
+  mutate(label_3 = NA,
+         label_4 = NA)
+df_threebox_3 <- df_threebox %>% 
+  filter(answerbox=="three") %>% 
+  select(lfdn, lhap_ec, answerbox, text, lseh2_31:lseh2_32) %>% 
+  rename(label_1 = lseh2_31,
+         label_2 = lseh2_32) %>% 
+  mutate(label_3 = NA,
+         label_4 = NA)
 
+df_threebox <- rbind(df_threebox_1,df_threebox_2,df_threebox_3)
 
-
-
-
-names(df)
-
-<- haven::as_factor(df$lhappro_c1_neu)
-
-
-df$lhappro1_c2
-
-
-lhappro2_c2
-lhappro3_c2
