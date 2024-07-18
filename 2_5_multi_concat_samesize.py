4beb9139d163eb6491c9683fae67822bb2322141
rubac <rl.bach@posteo.de>
2024-07-18 08:57:32 +0200

    gfdsa

--- 2_5_multi_concat_samesize.py
+++ 2_5_multi_concat_samesize.py
@@ -1,86 +1,51 @@
 # -*- coding: utf-8 -*-
 
-# install libraries
+
 # !pip install simpletransformers
-# !pip install scikit-learn
-# !pip install torch
-
-#### needs to be run again with appropriate data (i.e., 3box, 5box 10box)
-
-
-
-import pandas as pd
-import numpy as np
-import ast
-from simpletransformers.classification import MultiLabelClassificationModel, MultiLabelClassificationArgs
-from sklearn.metrics import  f1_score, accuracy_score, coverage_error
-from simpletransformers.classification import ClassificationModel, ClassificationArgs
+# !pip freeze | grep simpletransformers
 from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+from simpletransformers.classification import MultiLabelClassificationModel, MultiLabelClassificationArgs
+from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score, accuracy_score
+
 
 import torch
 cuda_available = torch.cuda.is_available()
+cuda_available
 
-# Multi-label metrics
-# input: np arrays: y_true contains dummies, prob contains probabilities (or already 1 / 0 labels)
-
-def zero_one_loss(y_true, prob):
-    y_pred=np.where(prob > threshold, 1, 0)
+def f1_multiclass(labels, preds):
+    return f1_score(labels, preds, average='micro')
+def convert_to_int(lst):
+    return [int(x) for x in lst]
+def replace_with_1(x):
+    return 1 if x != 0 else x
+def acc_zero_one_loss(y_true, y_pred):
+    if isinstance(y_true, pd.DataFrame):
+        y_true = np.array(y_true['labels'].tolist())
+    if isinstance(y_pred, pd.DataFrame):
+        y_pred = np.array(y_pred['labels'].tolist())
     nsample = len(y_true)
-    row_indicators = np.logical_not(np.all(y_true == y_pred, axis = 1)) # axis = 1 will check for equality along rows.
+    row_indicators = np.logical_not(np.all(y_true == y_pred, axis=1))
     not_equal_count = np.sum(row_indicators)
-    return not_equal_count/nsample
-
-def hamming_loss(y_true, prob):
-    y_pred=np.where(prob > threshold, 1, 0)
+    return 1 - (not_equal_count / nsample)
+def zero_one_loss(y_true, y_pred):
+    if isinstance(y_true, pd.DataFrame):
+        y_true = np.array(y_true['labels'].tolist())
+    if isinstance(y_pred, pd.DataFrame):
+        y_pred = np.array(y_pred['labels'].tolist())
+    nsample = len(y_true)
+    row_indicators = np.logical_not(np.all(y_true == y_pred, axis=1))
+    not_equal_count = np.sum(row_indicators)
+    return not_equal_count / nsample
+def hamming_loss(y_true, y_pred):
     hl_num = np.sum(np.logical_xor(y_true, y_pred))
-    hl_den = np.prod(y_true.shape)  # total n= #rows * #cols
-    return hl_num/hl_den
-
-# count number of predictions without any label (all below threshold)
-def fraction_zero_label(y_true, prob):
-    y_pred=np.where(prob > threshold, 1, 0)
-    nsample = len(y_true)
-    my_nlabels =  np.sum(y_pred,axis=1)
-    fraction = 1- (np.count_nonzero(my_nlabels) / nsample)
-    return fraction
-
-# count number of predictions with 1 label
-
-def fraction_one_label(y_true, prob):
-    y_pred=np.where(prob > threshold, 1, 0)
-    nsample = len(y_true)
-    my_nlabels =  np.sum(y_pred,axis=1)
-    np.count_nonzero((my_nlabels==1))
-    # counts the number of non-zero labels, but the only non zero number is 1
-    fraction = np.count_nonzero((my_nlabels==1)) / nsample
-    return fraction
-
-# count number of predictions with 2 labels
-def fraction_two_label(y_true, prob):
-    y_pred=np.where(prob > threshold, 1, 0)
-    nsample = len(y_true)
-    my_nlabels =  np.sum(y_pred,axis=1)
-    np.count_nonzero((my_nlabels==2))
-    # counts the number of non-zero labels, but the only non zero number is 1
-    fraction = np.count_nonzero((my_nlabels==2)) / nsample
-    return fraction
-
-# count number of predictions with 3 labels
-def fraction_three_label(y_true, prob):
-    y_pred=np.where(prob > threshold, 1, 0)
-    nsample = len(y_true)
-    my_nlabels =  np.sum(y_pred,axis=1)
-    np.count_nonzero((my_nlabels==2))
-    # counts the number of non-zero labels, but the only non zero number is 1
-    fraction = np.count_nonzero((my_nlabels==3)) / nsample
-    return fraction
-
-    from sklearn.metrics import  accuracy_score
-
-def av_labels_correct(labels, preds):
-    return accuracy_score(labels, np.round(preds))
+    hl_den = y_true.size  # total n = #rows * #cols
+    return hl_num / hl_den
 
 df = pd.read_csv(r'C:\Users\rbach\Documents\multilabel_ruben\data\all_concat.csv')
+
+
 df = df[df['exp_cond'] == 'ten box']
 df = df.drop(columns=['exp_cond'])
 
@@ -139,7 +104,7 @@ for split_index in range(100):
             model.train_model(train_df)
 
             # Evaluate the model on the validation set
-            val_result, val_model_outputs, val_wrong_predictions = model.eval_model(val_df, zero_one_loss=zero_one_loss, hamming_loss=hamming_loss, av_acc=av_labels_correct)
+            val_result, val_model_outputs, val_wrong_predictions = model.eval_model(val_df, zero_one_loss=zero_one_loss, hamming_loss=hamming_loss, av_acc=acc_zero_one_loss)
             av_acc_new = val_result["av_acc"]
             # Check if this combination is better than the previous best
             # We evaluate on average accuracy, which is no different from zero one loss (av_acc = 1 - zero_one_loss))
@@ -176,9 +141,8 @@ for split_index in range(100):
     
     # At end of each split, write test results to a CSV file
     test_results_df = pd.DataFrame(test_perf)
-    test_results_df.to_csv(r"C:\Users\rbach\Documents\multilabel_ruben\results\test_results_concat_tenbox.csv", index=False)
+    test_results_df.to_csv(r"C:\downloads\ruben_results\test_results_concat_tenbox.csv", index=False)
 
 # Final write to ensure all results are saved
 test_results_df = pd.DataFrame(test_perf)
-test_results_df.to_csv(r"C:\Users\rbach\Documents\multilabel_ruben\results\test_results_concat_tenbox.csv", index=False)
-
+test_results_df.to_csv(r"C:\downloads\ruben_results\test_results_concat_tenbox.csv", index=False)
